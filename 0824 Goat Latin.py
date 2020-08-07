d091039c44e7629883333ac00185dbc7baeade0a
AtharvRedij <atharvaredij@gmail.com>
2020-08-07 16:35:53 +0530

    Solution 824, 1455

---
+++ 0824 Goat Latin.py
@@ -0,0 +1,62 @@
+'''
+URL: https://leetcode.com/problems/goat-latin/
+
+Difficulty: Easy
+
+Description: Goat Latin
+
+A sentence S is given, composed of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+The rules of Goat Latin are as follows:
+
+If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+For example, the word 'apple' becomes 'applema'.
+ 
+If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+For example, the word "goat" becomes "oatgma".
+ 
+Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+Return the final sentence representing the conversion from S to Goat Latin. 
+
+ 
+
+Example 1:
+
+Input: "I speak Goat Latin"
+Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+Example 2:
+
+Input: "The quick brown fox jumped over the lazy dog"
+Output: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ 
+
+Notes:
+
+S contains only uppercase, lowercase and spaces. Exactly one space between each word.
+1 <= S.length <= 150.
+
+'''
+
+
+class Solution:
+    def toGoatLatin(self, S):
+        end_a_count = 1
+
+        output = ""
+
+        for word in S.split(" "):
+            if word[0].lower() in ['a', 'e', 'i', 'o', 'u']:
+                word += "ma"
+            else:
+                word = word[1:] + word[0] + "ma"
+
+            word += "a" * end_a_count
+
+            output += word + " "
+
+            end_a_count += 1
+
+        return output[:-1]
